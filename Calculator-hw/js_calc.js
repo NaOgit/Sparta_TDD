570797120d79fb567c9ea844b209d5d7bbe54807
NaO <noukongka@spartaglobal.com>
2018-08-08 16:26:40 +0100

    Test jasmine innitial stage

--- Calculator-hw/js_calc.js
+++ Calculator-hw/js_calc.js
@@ -9,35 +9,6 @@ function myCalculator(){
   //Switch calculators
   switch (calculatorOption) {
     case "b":
-      //Declared Basic calculator function
-      function myBasicCalculator(number1, number2, operator) {
-        //User input parameters
-        number1 = parseFloat(prompt("Enter a number"));
-        operator = prompt("Enter an operator");
-        number2 = parseFloat(prompt("Enter a number"));
-        //Switch operators
-        switch (operator) {
-          case "+":
-            //Return sum of
-            alert(("The answer is ") + (number1 + number2) + ("."));
-            break;
-          case "-":
-            //Return the subtracted
-            alert(("The answer is ") + (number1 - number2) + ("."));
-            break;
-          case "*":
-            //Return product of
-            alert(("The answer is ") + (number1 * number2) + ("."));
-            break;
-          case "/":
-            //Return the divided
-            alert(("The answer is ") + (number1 / number2) + ("."));
-            break;
-          default:
-            //If unexpected values are entered
-            alert("Please enter relevant values.");
-        }
-      }
       //Called function
       myBasicCalculator();
     break;
