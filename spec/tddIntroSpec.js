047db8bb16db664e1399155dc39fdbffd1575f52
NaO <noukongka@spartaglobal.com>
2018-08-08 19:06:09 +0100

    Changing code

--- spec/tddIntroSpec.js
+++ spec/tddIntroSpec.js
@@ -2,7 +2,7 @@ describe("Intro to doing TDD in javaScript", function(){
   var calculator = require("../lib/calcMeth");
 
   it("should add two numbers together", function(){
-    expect(calculator.addition(3,2)).toEqual(5);
+    expect(calculator.Addition(3,2)).toEqual(5);
   })
 
 })
