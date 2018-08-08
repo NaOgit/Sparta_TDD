c25cd37aaebc08f7288fd6baa7a5188211fabb7e
NaO <noukongka@spartaglobal.com>
2018-08-08 16:27:03 +0100

    Link test

--- spec/tddIntroSpec.js
+++ spec/tddIntroSpec.js
@@ -1,7 +1,8 @@
 describe("Intro to doing TDD in javaScript", function(){
-  var calculator = require("../lib/calcEngine");
+  var calculator = require("../lib/calcMeth");
+
   it("should add two numbers together", function(){
-    expect(calculator.addTwoNumbers(3,2)).toEqual(5);
+    expect(calculator.addition(3,2)).toEqual(5);
   })
 
 })
